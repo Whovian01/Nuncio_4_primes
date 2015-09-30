5ea88c7419921cad9a63b5fea935c067c49c25b8
Catrina Nuncio <Cat123@ninja9.d22.lan>
2015-09-30 07:44:03 -0500

    Hello fellow Whovian

---
+++ NetBeansProjects/Nuncio_4_primealgorithm/src/nuncio_4_primealgorithm/Nuncio_4_primealgorithm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuncio_4_primealgorithm;
+
+/**
+ *
+ * @author Cat123
+ */
+public class Nuncio_4_primealgorithm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        for(int i = 0; i < 100)
+    
+}
