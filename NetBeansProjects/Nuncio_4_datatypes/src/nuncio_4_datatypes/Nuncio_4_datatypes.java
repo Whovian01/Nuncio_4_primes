d4d9abb3455fa4f0acf4ec36b51184a1fe4985b7
Catrina Nuncio <Cat123@ninja9.d22.lan>
2015-10-05 07:14:23 -0500

    Hello fellow Whovian

---
+++ NetBeansProjects/Nuncio_4_datatypes/src/nuncio_4_datatypes/Nuncio_4_datatypes.java
@@ -0,0 +1,53 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuncio_4_datatypes;
+public class Nuncio_4_datatypes {
+    public static void main(String[]args) {
+//space for comments
+//used to explain code
+            //code explanation is an expectation
+            //in d22
+             int i =51;
+             float a = 7.11f;
+             double b = 11.0;
+             short c = 100;
+             char d1 = '1';
+             char d2 = 'd';
+             char d3 = 122;
+             
+             String e = "y";
+             boolean f = false;
+             boolean g = i % 1 == 0;
+             /*
+             here we will look at some mathematical operators
+             
+             */
+             System.out.println(i + "an integer");
+             System.out.println(i + "an integer");
+    }
+}
+/*% checks for the remainder
+// i % 2 checks if a number is 
+// even or odd */
+
+// a number value for char
+// maps to  character
+
+i = 22; 
+a = 0;
+e = "ninja";
+System.out.println(i + a + "an integer"
+        + "a string plus an integer"
+);
+/*adding int and a string
+//upcast to float is automatic 
+//i.e. 7 + 0.0 = 7.0
+
+
+double b = 11.0;
+             short c = 100;
+
+for 9i + 0; i < 500; i++) { 
